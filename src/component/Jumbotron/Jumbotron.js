5afa72d421d9298a7c6338717c0426da17aa1aca
Pickle Rick <dmryan711@gmail.com>
2019-06-07 01:25:46 -0400

    Fixed Image issue for deployment

--- src/component/Jumbotron/Jumbotron.js
+++ src/component/Jumbotron/Jumbotron.js
@@ -1,5 +1,4 @@
 import React from "react";
-import './style.css';
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Jumbotron(props) {
