b0d9610a55482e36d7907e98058d4aae4a9b9fe0
Pickle Rick <dmryan711@gmail.com>
2019-06-07 01:03:24 -0400

    Updates

---
+++ precache-manifest.bd6cb26acee47e60f852b63c99cb823e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91096c331aabb4ac7b2e95c47bd8e80f",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "70a2a85d34d159aabbce",
+    "url": "/ClickyGame/static/css/main.5409b4ef.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/ClickyGame/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "70a2a85d34d159aabbce",
+    "url": "/ClickyGame/static/js/main.c519803e.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
