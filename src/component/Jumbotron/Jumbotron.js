e9bcc238f3ef1eef48ad7d944cdb9c9180281b45
Pickle Rick <dmryan711@gmail.com>
2019-06-07 00:23:03 -0400

    Game scoring works

--- src/component/Jumbotron/Jumbotron.js
+++ src/component/Jumbotron/Jumbotron.js
@@ -4,9 +4,9 @@ import './style.css';
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Jumbotron(props) {
   return (
-    <div class="jumbotron mario">
-        <h1 class="display-4">Welcome!</h1>
-        <p class="lead">{props.message}</p>
+    <div className="jumbotron mario">
+        <h1 className="display-4">Welcome!</h1>
+        <p className="lead">{props.message}</p>
         <p>High Score: {props.highScore} </p> 
         <p>Score: {props.currentScore} </p>     
   </div>
--- src/util/images.js
+++ src/util/images.js
@@ -1,16 +1,16 @@
 const imageGallery = [
-    {id:1, src:'/images/Mario.jpg', title:'Mario'},
-    {id:2, src:'/images/Bowser.jpg', title:'Bowser'},
-    {id:3, src:'/images/BowserJR.jpg', title:'Bowser Jr.'},
-    {id:4, src:'/images/DK.jpg', title:'Donkey Kong'},
-    {id:5, src:'/images/DryBones.jpg', title:'Dry Bones'},
-    {id:6, src:'/images/KoopaTroopa.jpg', title:'Koopa Troopa'},
-    {id:7, src:'/images/Luigi.jpg', title:'Luigi'},
-    {id:8, src:'/images/Peach.jpg', title:'Peach'},
-    {id:9, src:'/images/Toad.jpg', title:'Toad'},
-    {id:10, src:'/images/Waluigi.jpg', title:'Waluigi'},
-    {id:11, src:'/images/Wario.jpg', title:'Wario'},
-    {id:12, src:'/images/Yoshi.jpg', title:'Yoshi'}
+    {id:"1", src:'/images/Mario.jpg', title:'Mario'},
+    {id:"2", src:'/images/Bowser.jpg', title:'Bowser'},
+    {id:"3", src:'/images/BowserJR.jpg', title:'Bowser Jr.'},
+    {id:"4", src:'/images/DK.jpg', title:'Donkey Kong'},
+    {id:"5", src:'/images/DryBones.jpg', title:'Dry Bones'},
+    {id:"6", src:'/images/KoopaTroopa.jpg', title:'Koopa Troopa'},
+    {id:"7", src:'/images/Luigi.jpg', title:'Luigi'},
+    {id:"8", src:'/images/Peach.jpg', title:'Peach'},
+    {id:"9", src:'/images/Toad.jpg', title:'Toad'},
+    {id:"10", src:'/images/Waluigi.jpg', title:'Waluigi'},
+    {id:"11", src:'/images/Wario.jpg', title:'Wario'},
+    {id:"12", src:'/images/Yoshi.jpg', title:'Yoshi'}
 ];
 
 export default imageGallery;
